03621db0a43c2bddf9f9f946380071332bee78a1
OCCHIO NERO <112170802+Baldesito@users.noreply.github.com>
2025-01-08 17:07:12 +0100

    3zo commit

--- src/Esercizio4/For.java
+++ src/Esercizio4/For.java
@@ -1,5 +1,15 @@
 package Esercizio4;
 
-public class For {
+import java.util.Scanner;
 
+public class For {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Inserisci un numero Intero: ");
+    int numero = scanner.nextInt();
+
+    for (int i = numero; i >= 0; i--){
+        System.out.println(i);
+    }
+}
 }
